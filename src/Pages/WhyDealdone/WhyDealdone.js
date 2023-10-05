5415ca92233ae61ef1f9999964a7a16a56057f64
Asfe <arafath@shafa.ai>
2023-10-05 19:07:40 +0600

    Why dealdone main title change

--- src/Pages/WhyDealdone/WhyDealdone.js
+++ src/Pages/WhyDealdone/WhyDealdone.js
@@ -11,25 +11,25 @@ const WhyDealdone = () => {
     {
       img: img1,
       title: "Multi-Channel Product Listing",
-      size:"400",
+      size: "400",
       text: "Expand from current channel onto many sales channels without a headache of manual listing.",
     },
     {
       img: img2,
       title: "Central Order Sync",
-      size:"280",
+      size: "280",
       text: "Instant sync to keep your stock up to date on always evolving inventory and avoiding marketplaces ban for overselling on unavailable stock.",
     },
     {
       img: img3,
       title: "All Orders In A Tab",
-      size:"245",
+      size: "245",
       text: "View all the orders from different channels in one place & easily track & ship them, without missing any important deadline - giving you complete control over your business.",
     },
     {
       img: img4,
       title: "Credit Score For Easy Loan",
-      size:"245",
+      size: "245",
       text: "Loans can be applied online in a click. We assess the data and create your credit profiles for our banking partners.",
     },
     // {
@@ -43,8 +43,9 @@ const WhyDealdone = () => {
       id="why_dealdone"
       className="px-3 md:px-10 min-h-fit my-14 md:my-0 p-5 "
     >
-      <SectionHeading title="Why Dealdone?" />
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-7 p-2">
+      <p style={{ textAlign: "center", fontSize: "20px",fontWeight:"700"}}>Why Dealdone ? </p>
+      <SectionHeading title="One Place. Multiple Platforms. All Gain. No Pain" />
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-7 p-2 mt-8">
         {items.map((item, i) => (
           <WhyDealdoneCard key={i} item={item} />
         ))}
